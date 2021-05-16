fb8291c5e05ff80b4dccca261bb40acd463e74a0
Daniel Jones <danielmjones1010@gmail.com>
2021-05-16 22:41:03 +0100

    cors policy changes

--- radical-tech-test-backend/Startup.cs
+++ radical-tech-test-backend/Startup.cs
@@ -31,11 +31,11 @@ namespace radical_tech_test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            app.UseCors(builder => builder
-   .AllowAnyOrigin()
-   .AllowAnyMethod()
-   .AllowAnyHeader()
-   .AllowCredentials());
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("https://danieljones94.github.io/radical-tech-test-frontend").AllowAnyHeader().AllowCredentials().AllowAnyMethod());
+            });
             services.AddDbContext<AppDbContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
@@ -63,7 +63,7 @@ namespace radical_tech_test
 
             app.UseRouting();
 
-            app.UseCors(o => o.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+            app.UseCors("AllowSpecificOrigin");
 
             app.UseAuthentication();
 
